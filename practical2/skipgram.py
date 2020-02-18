374917a2442f67ebd4bbbddb8b81196858de7617
Tom Lotze <tom.lotze@student.auc.nl>
2020-02-18 13:51:43 +0100

    class model, initialization almost complete

--- practical2/skipgram.py
+++ practical2/skipgram.py
@@ -6,6 +6,7 @@ import torch
 from read_ap import *
 import pickle as pkl 
 import os
+import torch.nn as nn
 
 docs_path = "./processed_docs.pkl"
 assert os.path.exists(docs_path), "Processed docs could not be found in this\
@@ -17,9 +18,56 @@ docs = get_processed_docs()
 # print example document
 # print(docs["AP891026-0263"])
 
-# set tunable parameters
-output_dim = 300
-window_size = 5
+
+class Skipgram(nn.Module):
+    def __init__(self, docs, output_dim=300, window_size=5):
+        """
+        Initialization of the skip gram model
+        Arguments:
+            - docs: documents on which the embeddings are learned
+            - output_dim: dimension of the embeddings that are learned
+            - window_size: window size in the skip gram model
+
+        """
+        super(MLP, self).__init__()
+
+        # set tunable parameters
+        self.output_dim = output_dim
+        self.window_size = 5
+        
+        self.docs = docs
+
+        self.layers = 
+
+
+
+    def forward():
+        """Perform a forward pass"""
+
+    def word_embedding(word):
+        pass
+        nn.Embedding
+
+
+
+
+
+    def doc_embedding(doc):
+        #aggregate word embeddings
+        pass
+
+
+
+
+if __name__ == "__main__":
+    docs_path = "./processed_docs.pkl"
+    assert os.path.exists(docs_path), "Processed docs could not be found in this\
+        directory. They will be processed now"
+
+    # docs is a dictionary with doc-ids as keys, value: lists of preprocessed words
+    docs = get_processed_docs()
+
+    skipgram_model = Skipgram(docs)
 
 
 
