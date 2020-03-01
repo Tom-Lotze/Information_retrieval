744f587fdc1654c03976a8dc0ea4ddcdc089afbd
Berend Jansen <bereeend@gmail.com>
2020-03-01 14:39:56 +0100

    updated file for lsa bow in lsa_bow.py

--- practical2/lsa_bow.py
+++ practical2/lsa_bow.py
@@ -23,62 +23,59 @@ def train(n_topics=num_topics):
     docs = [d for i, d in docs.items()]
 
     dictionary = corpora.Dictionary(docs)
-    dictionary.filter_extremes(no_below=50)
-
+    dictionary.filter_extremes(no_below=50
+                               )
     # save the dictionary
-    with open('./objects/dictionary_lsi_bin', 'wb') as f:
+    with open(os.path.join(folder_path_objects,
+                           'dictionary_lsi_bow'), 'wb') as f:
         pickle.dump(dictionary, f)
 
     # create binary and regular bow corpus
     corpus_bow = [dictionary.doc2bow(d) for d in docs]
     corpus_binary = [[(i, 1) for i, _ in d] for d in corpus_bow]
 
-    # # save corpuses
-    with open(os.path.join(folder_path_objects, 'corpus_binary_lsi'), 'wb') as f:
+    # save corpuses
+    with open(os.path.join(folder_path_objects,
+                           'corpus_binary'), 'wb') as f:
         pickle.dump(corpus_binary, f)
 
     # create models
-    # print(f'{time.ctime()} Start training LSA (binary bow)')
-    # lsi_bin = LsiModel(
-    #     corpus=corpus_binary,
-    #     id2word=dictionary,
-    #     chunksize=1000,
-    #     num_topics=n_topics
-    # )
+    print(f'{time.ctime()} Start training LSA (binary bow)')
+    lsi_bin = LsiModel(
+        corpus=corpus_binary,
+        id2word=dictionary,
+        chunksize=1000,
+        num_topics=n_topics
+    )
 
-    # # save models to disk
-    # os.makedirs(folder_path_models, exist_ok=True)
+    # save models to disk
+    os.makedirs(folder_path_models, exist_ok=True)
 
-    # lsi_bin.save('./models/lsi_bin_filtered')
+    lsi_bin.save('./models/lsi_bin_filtered')
 
 
 def create_index(model):
+    assert model in ['lsi_bin']
 
-    with open('./objects/corpus_binary_lsi', 'rb') as f:
-        corpus = pickle.load(f)
+    corpus = get_corpus('binary')
 
-    m = LsiModel.load('./models/lsi_bin_filtered')
-
-    with open('./objects/dictionary_lsi_bin', 'rb') as f:
-        dictionary = pickle.load(f)
+    m = get_model(model)
+    dictionary = get_dictionary()
 
     index = similarities.SparseMatrixSimilarity(
         m[corpus], num_features=len(dictionary.token2id))
 
-    index.save('./objects/index_lsi_bin_filtered')
+    filepath_out = os.path.join(folder_path_objects, f'index_{model}')
+
+    index.save(filepath_out)
 
 
 # helper functions
 def get_index(model_type, num_topics):
-    assert model_type in ['lsi_bin', 'lsi_tfidf', 'lda_tfidf']
-
-    # if 'tfidf' in model:
-    #     corpus = get_corpus('tfidf')
-    # else:
-    #     corpus = get_corpus('binary')
+    assert model_type in ['lsi_bin']
 
     filepath_in = os.path.join(
-        folder_path_objects, f'index_{model_type}_filtered')
+        folder_path_objects, f'index_{model_type}_{num_topics}')
     index = similarities.MatrixSimilarity.load(filepath_in)
 
     return index
@@ -90,27 +87,22 @@ def get_dictionary():
 
 
 def get_corpus(corpus):
-    assert corpus in ['binary', 'tfidf']
-
-    with open(os.path.join(folder_path_objects, f'corpus_{corpus}'), 'rb') as f:
+    with open(os.path.join(folder_path_objects, f'corpus_binary'), 'rb') as f:
         return pickle.load(f)
 
 
 def get_model(model, num_topics=500):
-    assert model in ['lsi_bin', 'lsi_tfidf', 'lda_tfidf']
+    assert model in ['lsi_bin']
 
-    filepath = os.path.join(folder_path_models, f'{model}_{num_topics}')
+    filepath = os.path.join(folder_path_models, f'lsi_bin_{num_topics}')
 
-    if 'lsi' in model:
-        return LsiModel.load(filepath)
-    else:
-        return LdaModel.load(filepath)
+    return LsiModel.load(filepath)
 
 
 class Search:
 
     def __init__(self, model, model_type, num_topics):
-        assert model_type in ['lsi_bin', 'lsi_tfidf', 'lda_tfidf']
+        assert model_type in ['lsi_bin']
 
         self.index = get_index(model_type, num_topics)
         self.dictionary = get_dictionary()
@@ -118,13 +110,13 @@ class Search:
 
     def query(self, q):
 
-        # assert model in ['lsi_bin', 'lsi_tfidf', 'lda_tfidf']
+        # assert model in ['lsi_bin']
 
         # get doc representation
         q = read_ap.process_text(q)
         q = self.dictionary.doc2bow(q)
 
-        # print(f"q is : {q}")
+        #print(f"q is : {q}")
 
         # convert vector to LSI space
         vec_query = self.model[q]
@@ -147,5 +139,3 @@ if __name__ == '__main__':
 
     # create indices
     create_index('lsi_bin')
-    # create_index('lsi_tfidf')
-    # create_index('lda_tfidf')
