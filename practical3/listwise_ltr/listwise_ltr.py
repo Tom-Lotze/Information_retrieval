611a055a0059d651cac78b8f5aeb464a0332f7f2
Berend Jansen <bereeend@gmail.com>
2020-03-04 17:53:13 +0100

    init template pytorch

---
+++ practical3/listwise_ltr/listwise_ltr.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+class Listwise(nn.module):
+    """ Listwise LTR model """
+
+    def __init__(self):
+        """ Initialize model """
+        super(Listwise, self).__init__()
+
+    def forward(self, x):
+        """ Forward pass """
+
+
+if __name__ == "__main__":
+    pass
---
+++ practical3/pairwise_ltr/pairwise_ltr.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+class Pairwise(nn.module):
+    """ Pairwise LTR model """
+
+    def __init__(self):
+        """ Initialize model """
+        super(Pairwise, self).__init__()
+
+    def forward(self, x):
+        """ Forward pass """
+
+
+if __name__ == "__main__":
