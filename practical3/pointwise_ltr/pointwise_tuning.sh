ce27fcf35b68bf00ba2e0d667b1744e25ab0995b
Tom Lotze <tom.lotze@student.auc.nl>
2020-03-07 12:33:06 +0100

    Gridsearch script for pointwise

---
+++ practical3/pointwise_ltr/pointwise_tuning.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source activate IR1-hw2
+
+echo "Starting gridsearch"
+
+declare -a hidden_list=("128, 128, 128" "512, 256 ,128 " "256, 10")
+declare -a lr_list=("0.001" "0.01" "0.005")
+
+
+
+for h_u in "${hidden_list[@]}"
+do
+    for lr in "${lr_list[@]}"
+    do
+        echo "hidden units: $h_u, learning rate: $lr"
+        python pointwise_ltr/pointwise_ltr.py --hidden_units $h_u\
+         --learning_rate $lr \
+         >> pointwise_ltr/json_files/gridsearch_results.txt
+    done
+done
+
