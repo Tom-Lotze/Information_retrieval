7dfe68e6c03f30cb8296c13f13ca47870b8c6af0
Ruth Wijma <rlwijma@gmail.com>
2020-03-09 12:33:32 +0100

    ltr begin

--- practical3/listwise_ltr/listwise_ltr.py
+++ practical3/listwise_ltr/listwise_ltr.py
@@ -3,15 +3,23 @@ import torch.nn as nn
 import numpy as np
 
 
-class Listwise(nn.module):
+class Listwise(nn.Module):
     """ Listwise LTR model """
 
-    def __init__(self):
+    def __init__(self, input_size=501):
         """ Initialize model """
         super(Listwise, self).__init__()
 
+        self.nnet = nn.Sequential(
+            nn.Linear(input_size, 20),
+            nn.ReLU(),
+            nn.Linear(20,1),
+            nn.ReLU()
+        )
+
     def forward(self, x):
         """ Forward pass """
+        return self.nnet(x)
 
 
 if __name__ == "__main__":
