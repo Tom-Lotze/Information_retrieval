dac0fc64601a815f216c7b628dc16367604c6e7d
Tom Lotze <tom.lotze@student.auc.nl>
2020-03-09 14:45:32 +0100

    cleanup

--- practical3/pointwise_ltr/process_jsons.py
+++ practical3/pointwise_ltr/process_jsons.py
@@ -22,22 +22,6 @@ def process_json():
         ndcg_at_40[file] = scores_dict["39"]["ndcg"][0]
 
 
-        # avg_map_all = np.mean([scores_dict[query_scores]["map"] for query_scores in
-        #     scores_dict])
-        # avg_map_76_100 = np.mean([scores_dict[query_scores]["map"] for query_scores in
-        #     scores_dict if int(query_scores) in range(76, 101)])
-        # avg_ndcg_all = np.mean([scores_dict[query_scores]["ndcg"] for query_scores in
-        #     scores_dict])
-        # avg_ndcg_76_100 = np.mean([scores_dict[query_scores]["ndcg"] for query_scores in
-        #     scores_dict if int(query_scores) in range(76, 101)])
-
-        # print(f"avg_map_all: {avg_map_all:.4f}")
-        # print(f"avg_map_76-100: {avg_map_76_100:.4f}")
-        # print(f"avg_ndcg_all: {avg_ndcg_all:.4f}")
-        # print(f"avg_ndcg_76-100: {avg_ndcg_76_100:.4f}")
-
-    #print(ndcg_at_40)
-
     max_ndcg_40_key = max(ndcg_at_40.keys(), key=lambda key: ndcg_at_40[key])
     max_value = ndcg_at_40[max_ndcg_40_key]
     average_ndcg_40 = np.mean(list(ndcg_at_40.values()))
