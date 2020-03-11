ecc48b9ac8f0a148b84aa4d857ec32f4a18f4295
Berend Jansen <bereeend@gmail.com>
2020-03-11 21:50:49 +0100

    running but very slow

--- practical3/pairwise_ltr/pairwise_ltr.py
+++ practical3/pairwise_ltr/pairwise_ltr.py
@@ -32,17 +32,24 @@ class RankNet(nn.Module):
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
-    def forward(self, x):
+    def forward(self, d_i, d_j):
         """
         Forward pass
         - x: batch of document pairs B x (doc_i, doc_j)
         - out: batch of document scores: B x 1
         """
+        # d_i = pairs[:, 0, :]
+        # d_j = pairs[:, 1, :]
 
-        h_i = self.relu(self.fc1(x))
+        h_i = self.relu(self.fc1(d_i))
         s_i = self.sigmoid(self.fc2(h_i))
 
-        return s_i
+        h_j = self.relu(self.fc1(d_j))
+        s_j = self.sigmoid(self.fc2(h_j))
+
+        diff = s_i - s_j
+
+        return diff
 
 
 def weights_init(model):
@@ -51,6 +58,12 @@ def weights_init(model):
         nn.init.xavier_uniform_(model.weight.data)
         if model.bias is not None:
             torch.nn.init.zeros_(model.bias)
+
+
+def loss_function(S, diffs):
+    loss = (1/2) * (1 - S) * diffs + \
+        torch.log(1 + torch.exp(-1 * diffs))
+    return loss
 
 
 def train(data):
@@ -69,55 +82,59 @@ def train(data):
 
         with tqdm(total=len(train_dl)) as t:
             t.set_description(f'Epoch: {epoch+1}/{n_epochs}')
+
             for step, (x_batch, y_batch) in enumerate(train_dl):
+
                 optimizer.zero_grad()
 
                 # ignore batch if only one doc (no pairs)
                 if x_batch.shape[1] == 1:
                     continue
 
+                x_batch = x_batch.float().squeeze()
                 # squeeze batch
+
                 y_batch = y_batch.squeeze()
-
-                # compute scores per doc
-                y_hat = model(x_batch.float()).squeeze()
-
-                # compute score diffs per doc pair
-                y_hat_diffs = Variable(torch.Tensor([(s_i - s_j)
-                                                     for i, s_i in enumerate(y_hat)
-                                                     for j, s_j in enumerate(y_hat)
-                                                     if i != j]), requires_grad=True)
-
-                # label pairs
-                y_pairs = torch.Tensor([(s_i, s_j)
+                y_pairs = torch.Tensor([[s_i, s_j]
                                         for i, s_i in enumerate(y_batch)
                                         for j, s_j in enumerate(y_batch)
                                         if i != j])
 
+                x_pairs = torch.Tensor([[d_i.numpy(), d_j.numpy()] for i, d_i in enumerate(
+                    x_batch) for j, d_j in enumerate(x_batch) if i != j])
+
+                x_i = x_pairs[:, 0, :]
+                x_j = x_pairs[:, 1, :]
+
+                y_i = y_pairs[:, 0]
+                y_j = y_pairs[:, 1]
+
+                diffs = model(x_i, x_j)
+
                 # compute real diffs
-                S_ij = torch.zeros_like(y_hat_diffs)
-                for s, (i, j) in enumerate(y_pairs):
-                    if i > j:
-                        S_ij[s] = 1
-                    elif i == j:
-                        S_ij[s] = 0
-                    elif i < j:
-                        S_ij[s] = -1
+                S_ij = torch.zeros_like(diffs)
+                S_ij[y_i > y_j] = 1
+                S_ij[y_j > y_j] = -1
 
                 # define loss function
-                # sigma = 1
-                loss = (1/2) * (1 - S_ij) * y_hat_diffs + \
-                    torch.log(1 + torch.exp(-1 * y_hat_diffs))
+                loss = loss_function(S_ij, diffs)
+
+                # Variable((1/2) * (1 - S_ij) * y_hat_diffs +
+                # torch.log(1 + torch.exp(-1 * y_hat_diffs)),
+                # requires_grad=True)
+
+                if step % 50 == 0:
+                    t.set_postfix_str(f'loss: {loss.mean().item()}')
 
                 # take mean to compare between batches
-                loss = torch.mean(loss)
+                loss = loss.sum()
 
                 # backward pass
                 loss.backward()
+
+                # optimizer step
                 optimizer.step()
 
-                if step % 10 == 0:
-                    t.set_postfix_str(f'loss: {loss.item()}')
                 t.update()
 
 
