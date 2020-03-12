f6f83d27f36ba05b57182aa09876e6edb04a93ef
Berend Jansen <bereeend@gmail.com>
2020-03-12 16:12:39 +0100

    updated, yet to implement ndcg

--- practical3/pairwise_ltr/pairwise_ltr.py
+++ practical3/pairwise_ltr/pairwise_ltr.py
@@ -5,6 +5,7 @@ from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 from torch.autograd import Variable
 import torch
+
 import time
 sys.path.append('..')
 sys.path.append('.')
@@ -32,7 +33,7 @@ class RankNet(nn.Module):
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
-    def forward(self, d_i, d_j):
+    def forward(self, x_batch):
         """
         Forward pass
         - x: batch of document pairs B x (doc_i, doc_j)
@@ -41,15 +42,49 @@ class RankNet(nn.Module):
         # d_i = pairs[:, 0, :]
         # d_j = pairs[:, 1, :]
 
-        h_i = self.relu(self.fc1(d_i))
-        s_i = self.sigmoid(self.fc2(h_i))
+        h_i = self.relu(self.fc1(x_batch))
+        s_i = self.relu(self.fc2(h_i))
 
-        h_j = self.relu(self.fc1(d_j))
-        s_j = self.sigmoid(self.fc2(h_j))
+        return s_i
 
-        diff = s_i - s_j
+    def evaluate_on_validation(self, valid_dl):
+        """ evaluate on validation """
+        with torch.no_grad():
+            averages = []
+            predictions_list = []
 
-        return diff
+            for step, (x_step, y_step) in enumerate(valid_dl):
+                pass
+
+
+class Loss_function(nn.Module):
+    """ Pairwise LTR model """
+
+    def __init__(self, gamma=1):
+        """ Loss function init """
+        super(Loss_function, self).__init__()
+        self.gamma = gamma
+
+    def forward(self, y_hat, y):
+        """
+        y_hat: 1d tensor of document scores
+        """
+
+        diff_mat = Variable(torch.sigmoid(y_hat.repeat(
+            y_hat.shape[0], 1).t() - y_hat), requires_grad=True)
+
+        labels_mat = y.repeat(y.shape[0], 1).t() - y
+
+        labels_mat[labels_mat > 0] = 1
+        labels_mat[labels_mat == 0] = (1/2)
+        labels_mat[labels_mat < 0] = 0
+
+        # loss = (1/2) * (1 - labels_mat) * self.gamma * diff_mat + \
+        # torch.log(1+torch.exp(-self.gamma * diff_mat))
+
+        loss = nn.functional.binary_cross_entropy(diff_mat, labels_mat)
+
+        return loss
 
 
 def weights_init(model):
@@ -58,12 +93,6 @@ def weights_init(model):
         nn.init.xavier_uniform_(model.weight.data)
         if model.bias is not None:
             torch.nn.init.zeros_(model.bias)
-
-
-def loss_function(S, diffs):
-    loss = (1/2) * (1 - S) * diffs + \
-        torch.log(1 + torch.exp(-1 * diffs))
-    return loss
 
 
 def train(data):
@@ -75,8 +104,13 @@ def train(data):
     train_dataset = dataset.ListDataSet(data.train)
     train_dl = DataLoader(train_dataset)
 
+    valid_dataset = dataset.ListDataSet(data.validation)
+    valid_dl = DataLoader(valid_dataset)
+
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     step = 0
+
+    loss_function = Loss_function(gamma=1)
 
     for epoch in range(n_epochs):
 
@@ -91,43 +125,51 @@ def train(data):
                 if x_batch.shape[1] == 1:
                     continue
 
+                if step > 5:
+                    continue
+
                 x_batch = x_batch.float().squeeze()
                 # squeeze batch
 
-                y_batch = y_batch.squeeze()
-                y_pairs = torch.Tensor([[s_i, s_j]
-                                        for i, s_i in enumerate(y_batch)
-                                        for j, s_j in enumerate(y_batch)
-                                        if i != j])
+                y_batch = y_batch.squeeze().float()
 
-                x_pairs = torch.Tensor([[d_i.numpy(), d_j.numpy()] for i, d_i in enumerate(
-                    x_batch) for j, d_j in enumerate(x_batch) if i != j])
+                # y_pairs = torch.Tensor([[s_i, s_j]
+                # for i, s_i in enumerate(y_batch)
+                # for j, s_j in enumerate(y_batch)
+                # if i != j])
 
-                x_i = x_pairs[:, 0, :]
-                x_j = x_pairs[:, 1, :]
+                # x_pairs = torch.Tensor([[d_i.numpy(), d_j.numpy()] for i, d_i in enumerate(
+                # x_batch) for j, d_j in enumerate(x_batch) if i != j])
 
-                y_i = y_pairs[:, 0]
-                y_j = y_pairs[:, 1]
+                # x_i = x_pairs[:, 0, :]
+                # x_j = x_pairs[:, 1, :]
 
-                diffs = model(x_i, x_j)
+                # y_i = y_pairs[:, 0]
+                # y_j = y_pairs[:, 1]
+
+                scores = model(x_batch).squeeze()
 
                 # compute real diffs
-                S_ij = torch.zeros_like(diffs)
-                S_ij[y_i > y_j] = 1
-                S_ij[y_j > y_j] = -1
+                # S_ij = torch.zeros_like(diffs)
+                # S_ij[y_i > y_j] = 1
+                # S_ij[y_j > y_j] = -1
 
                 # define loss function
-                loss = loss_function(S_ij, diffs)
+                loss = loss_function(scores, y_batch)
 
                 # Variable((1/2) * (1 - S_ij) * y_hat_diffs +
                 # torch.log(1 + torch.exp(-1 * y_hat_diffs)),
                 # requires_grad=True)
 
-                if step % 50 == 0:
-                    t.set_postfix_str(f'loss: {loss.mean().item()}')
+                if step % 1 == 0:
+                    # model.eval()
+
+                    # ndcg_10 = evl.ndcg_at_k(sorted_labels, 10)
+                    t.set_postfix_str(f'loss: {loss.mean().item():3f}')
+                    # model.train()
 
                 # take mean to compare between batches
-                loss = loss.sum()
+                # loss = loss.sum()
 
                 # backward pass
                 loss.backward()
@@ -142,6 +184,7 @@ if __name__ == "__main__":
     # currently importing dataset here as pep8 would move it to the
     # top messing up the sys.path code
     import dataset
+    import evaluate as evl
 
     # # load and read data
     data = dataset.get_dataset().get_data_folds()[0]
