d112f7762a6a33cdd6938903b7836ab7c9f57b39
Tom Lotze <tom.lotze@student.auc.nl>
2020-03-13 19:36:09 +0100

    new test set formats can be analyzed

--- practical3/pointwise_ltr/process_jsons.py
+++ practical3/pointwise_ltr/process_jsons.py
@@ -5,32 +5,32 @@ from scipy.stats import ttest_rel
 from itertools import combinations
 
 def process_json():
-
-
-
-
     curr_path = os.path.join(os.getcwd(), "pointwise_ltr/json_files/")
 
     json_files = [file for file in os.listdir(curr_path) if file.endswith(".json")]
     #print(json_files)
-    ndcg_at_40 = dict()
+    ndcg_on_test = dict()
 
     for file in json_files:
-        #print("\n"+file)
+        if not "TEST" in file:
+            continue
+        print("\n"+file)
         with open(os.path.join(curr_path, file)) as f:
             scores_dict = json.load(f)
-        ndcg_at_40[file] = scores_dict["39"]["ndcg"][0]
+        ndcg_score = scores_dict["ndcg"][0]
+        print(f"nDCG score: {ndcg_score}")
+        ndcg_on_test[file] = ndcg_score
 
 
-    max_ndcg_40_key = max(ndcg_at_40.keys(), key=lambda key: ndcg_at_40[key])
-    max_value = ndcg_at_40[max_ndcg_40_key]
-    average_ndcg_40 = np.mean(list(ndcg_at_40.values()))
-    var_ndcg_40 = np.var(list(ndcg_at_40.values()))
+    max_ndcg_40_key = max(ndcg_on_test.keys(), key=lambda key: ndcg_on_test[key])
+    max_value = ndcg_on_test[max_ndcg_40_key]
+    average_ndcg_40 = np.mean(list(ndcg_on_test.values()))
+    var_ndcg_40 = np.var(list(ndcg_on_test.values()))
 
     return max_ndcg_40_key, max_value, average_ndcg_40, var_ndcg_40
 
 if __name__ == "__main__":
     best_setting, best_ndcg, average, variance = process_json()
-    print(f"best config: {best_setting}\nNDCG score at 40 epochs: {best_ndcg}\naverage NDCG at 40: {average}\nNDCG variance: {variance}")
+    print(f"\nbest config: {best_setting}\nNDCG score on test: {best_ndcg}\naverage NDCG on test: {average}\nNDCG variance: {variance}")
 
 
