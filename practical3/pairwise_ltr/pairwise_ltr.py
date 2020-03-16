61c4d3474113fa5f82de2da3882edd8f0ef5985d
Berend Jansen <bereeend@gmail.com>
2020-03-16 11:39:25 +0100

    pairwise update to work like pointwise

--- practical3/pairwise_ltr/pairwise_ltr.py
+++ practical3/pairwise_ltr/pairwise_ltr.py
@@ -1,14 +1,15 @@
 import sys
 import numpy as np
 import torch.nn as nn
-from torchviz import make_dot
 import matplotlib.pyplot as plt
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 from torch.autograd import Variable
 import torch
+import argparse
+import json
+import os
 
-import time
 sys.path.append('..')
 sys.path.append('.')
 
@@ -41,9 +42,7 @@ class RankNet(nn.Module):
         - x: batch of document pairs B x (doc_i, doc_j)
         - out: batch of document scores: B x 1
         """
-        # d_i = pairs[:, 0, :]
-        # d_j = pairs[:, 1, :]
-        # print(x_batch.shape)
+
         h_i = self.sigmoid(self.fc1(x_batch))
         s_i = self.sigmoid(self.fc2(h_i))
 
@@ -68,39 +67,15 @@ class RankNet(nn.Module):
             results = evl.evaluate(valid_data, valid_scores)
         return results
 
-
-class Loss_function(nn.Module):
-    """ Pairwise LTR model """
-
-    def __init__(self, gamma=1):
-        """ Loss function init """
-        super(Loss_function, self).__init__()
-        self.gamma = gamma
-
-    def forward(self, y_hat, y):
-        """
-        y_hat: 1d tensor of document scores
-        """
-
-        # diff_mat = Variable(torch.sigmoid(y_hat.expand(
-        # y_hat.shape[0], y_hat.shape[0]).t() - y_hat), requires_grad=True)
-
-        diff_mat = Variable(torch.sigmoid(
-            torch.add(y_hat.t(), -y_hat)), requires_grad=True)
-
-        labels_mat = y.repeat(y.shape[0], 1).t() - y
-
-        labels_mat[labels_mat > 0] = 1
-        labels_mat[labels_mat == 0] = (1/2)
-        labels_mat[labels_mat < 0] = 0
-
-        loss = nn.functional.binary_cross_entropy(diff_mat, labels_mat)
-
-        return loss
-
-    def backward(self):
-        """ backward pass sped up ranknet """
-        pass
+    def evaluate_on_test(self, data):
+        """ Evaluate on test set """
+        test_data = data.test
+        with torch.no_grad():
+            test_scores = self.single_foward(
+                torch.Tensor(test_data.feature_matrix))
+            test_scores = test_scores.numpy().squeeze()
+            results = evl.evaluate(test.data, test_scores)
+        return results
 
 
 def weights_init(model):
@@ -111,8 +86,12 @@ def weights_init(model):
             torch.nn.init.zeros_(model.bias)
 
 
-def train(data):
-    model = RankNet(data.num_features, 256, 1)
+def train(data, FLAGS):
+
+    n_hidden = FLAGS.number_hidden
+    n_epochs = FLAGS.max_epochs
+
+    model = RankNet(data.num_features, n_hidden, 1)
     model.apply(weights_init)
 
     n_epochs = 10
@@ -120,15 +99,22 @@ def train(data):
     train_dataset = dataset.ListDataSet(data.train)
     train_dl = DataLoader(train_dataset)
 
-    # valid_dataset = dataset.ListDataSet(data.validation)
-    # valid_dl = DataLoader(valid_dataset)
+    optimizer = torch.optim.Adam(model.parameters(), lr=FLAGS.learning_rate)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-    # loss_function = Loss_function(gamma=1)
     loss_function = nn.BCELoss(reduction='mean')
 
+    training_losses = []
+    validation_results = {}
+    ndcg_per_epoch = {}
+
+    filename_validation_results = f"./pairwise_ltr/json_files/pairwise_{n_hidden}_{FLAGS.learning_rate}.json"
+    filename_test_results = f"./pairwise_ltr/json_files/pairwise_TEST_{n_hidden}_{FLAGS.learning_rate}.json"
+    figure_name = f"{n_hidden}_{FLAGS.learning_rate}.png"
+
+    overall_step = 0
+
     for epoch in range(n_epochs):
+        ndcg_per_epoch[epoch] = {}
 
         with tqdm(total=len(train_dl)) as t:
             t.set_description(f'Epoch: {epoch+1}/{n_epochs}')
@@ -141,12 +127,8 @@ def train(data):
                 if x_batch.shape[1] == 1:
                     continue
 
-                # if step > 5:
-                #     continue
-
-                x_batch = x_batch.float().squeeze()
                 # squeeze batch
-
+                x_batch = x_batch.float().squeeze()
                 y_batch = y_batch.float().t()
 
                 labels_mat = y_batch.t() - y_batch
@@ -159,20 +141,14 @@ def train(data):
 
                 loss = loss_function(diff_scores, labels_mat)
 
-                # define loss function
-                # loss = loss_function(scores, y_batch)
-
-                if step % 5 == 0:
-                    # model.eval()
-
+                if overall_step % FLAGS.valid_each == 0:
+                    model.eval()
                     valid_results = model.evaluate_on_validation(data)
-                    # valid_results = {'ndcg@20': (0.5, 0.4)}
-                    # ndcg_10 = evl.ndcg_at_k(sorted_labels, 10)
+                    validation_results[overall_step] = valid_results
+                    ndcg_per_epoch[epoch][step] = valid_results['ndcg'][0]
+                    training_losses.append(loss.item)
                     t.set_postfix_str(
                         f'loss: {loss.mean().item():3f} ndcg: {valid_results["ndcg"][0]:3f}')
-
-                # take mean to compare between batches
-                # loss = loss.sum()
 
                 # backward pass
                 loss.backward()
@@ -181,6 +157,63 @@ def train(data):
                 optimizer.step()
 
                 t.update()
+
+                overall_step += 1
+
+            if epoch % 1 == 0 and FLAGS.save:
+                filename_model = (f"./pairwise_ltr/models/pairwise_{n_hidden}_"
+                                  "{epoch}_{FLAGS.learning_rate}.pt")
+                torch.save(model.state_dict(), filename_model)
+                print(f"Model is saved as {filename_model}")
+
+            # early stopping
+            if epoch > 0:
+                mean_prev_epoch = np.mean(
+                    list(ndcg_per_epoch[epoch-1].values()))
+                mean_curr_epoch = np.mean(list(ndcg_per_epoch[epoch].values()))
+                print(mean_prev_epoch, mean_curr_epoch)
+                if (mean_curr_epoch <= mean_prev_epoch +
+                        FLAGS.early_stopping_threshold):
+                    print("Early stopping condition satistied, "
+                          "stopping training")
+                    break
+
+    # save results
+    if FLAGS.plot:
+        plot_loss_ndcg(validation_results, training_losses, figure_name)
+
+    if FLAGS.save:
+        with open(filename_validation_results, "w") as writer:
+            json.dump(validation_results, writer, indent=1)
+        with open(filename_test_results, "w") as writer:
+            json.dump(model.evaluate_on_test(data, FLAGS), writer, indent=1)
+        print(f"Results are saved in the json_files folder")
+
+
+def plot_loss_ndcg(ndcg, loss, figname):
+    ndcg_values = [i["ndcg"][0] for i in ndcg.values()]
+    x_labels = list(ndcg.keys())
+
+    fig, ax1 = plt.subplots()
+
+    color = 'tab:red'
+    ax1.set_xlabel('Batch (1 query per batch)')
+    ax1.set_ylabel('nDCG', color=color)
+    ax1.plot(x_labels, ndcg_values, color=color, label="nDCG")
+    ax1.tick_params(axis='y', labelcolor=color)
+    ax1.legend(loc=0)
+
+    ax2 = ax1.twinx()
+
+    color = 'tab:blue'
+    ax2.set_ylabel('Training loss', color=color)
+    ax2.plot(x_labels, loss, color=color, label="Training loss")
+    ax2.tick_params(axis='y', labelcolor=color)
+    ax2.legend(loc=1)
+
+    plt.title("nDCG and training loss for Pairwise LTR")
+    plt.tight_layout()
+    plt.savefig(f"pairwise_ltr/figures/{figname}")
 
 
 if __name__ == "__main__":
@@ -195,6 +228,36 @@ if __name__ == "__main__":
     data = dataset.get_dataset().get_data_folds()[0]
     data.read_data()
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--hidden_units', type=str, default="256, 10",
+                        help=('Comma separated list of unit numbers in each '
+                              'hidden layer'))
+    parser.add_argument('--learning_rate', type=float,
+                        default=0.001, help='Learning rate')
+    parser.add_argument('--max_epochs', type=int,
+                        default=40, help='Max number of epochs')
+    parser.add_argument("--save", type=int, default=1,
+                        help=("Either 1 or 0 (bool) to save"
+                              " the model and results"))
+    parser.add_argument("--plot", type=int, default=0,
+                        help=("Either 1 or 0 (bool) to create a plot "
+                              "of ndcg and loss"))
+    parser.add_argument("--valid_each", type=int, default=100,
+                        help=("Run the model on the validation "
+                              "set every x steps"))
+    parser.add_argument("--early_stopping_threshold", type=float, default=0.0,
+                        help=("Minimal difference in ndcg on validation "
+                              "set between epochs to continue training"))
+    parser.add_argument("--save_pred", type=int, default=0,
+                        help=("Boolean (0, 1) whether to save the "
+                              "predictions on the test set"))
+
+    # set configuration in FLAGS parameter
+    FLAGS, unparsed = parser.parse_known_args()
+    FLAGS.save = bool(FLAGS.save)
+    FLAGS.plot = bool(FLAGS.plot)
+    FLAGS.save_pred = bool(FLAGS.save_pred)
+
     # # print data stats
     print('Number of features: %d' % data.num_features)
     print('Number of queries in training set: %d' % data.train.num_queries())
@@ -206,6 +269,10 @@ if __name__ == "__main__":
     print('Number of queries in test set: %d' % data.test.num_queries())
     print('Number of documents in test set: %d' % data.test.num_docs())
 
-    # train Model
+    # create necessary datasets
+    os.makedirs("pairwise_ltr/models", exist_ok=True)
+    os.makedirs("pairwise_ltr/json_files", exist_ok=True)
+    os.makedirs("pairwise_ltr/figures", exist_ok=True)
 
-    train(data)
+    # train model
+    train(data, FLAGS)
